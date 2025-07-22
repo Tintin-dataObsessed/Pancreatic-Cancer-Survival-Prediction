4378fa53289c8fcc68774119212c354180c427f1
chriskarta <chris10karta@gmail.com>
2025-07-22 23:10:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,4 @@
-<<<<<<< HEAD
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "7de64844-22ef-456a-9793-2051055cc448",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Pyhton (learn-env)",
-   "language": "python",
-   "name": "learn-env"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.12.7"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
-=======
-import streamlit as st 
-import joblib
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+import pickle
+with open('scaler.pkl', 'rb') as f:
+    print(pickle.load(f))
 
-
-st.title('🧬 PANCREATIC CANCER PREDICTION APP')
-
->>>>>>> de9041523836f9aa0b5bca4cc4b67e7bd4f4152e
