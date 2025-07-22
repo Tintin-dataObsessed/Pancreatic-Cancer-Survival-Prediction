6cdf3aa332c7fa06541f6c4d7f33f1469ab41012
chriskarta <chris10karta@gmail.com>
2025-07-22 22:37:11 +0300

    changes made to the app

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
 {
- "cells": [],
- "metadata": {},
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "7de64844-22ef-456a-9793-2051055cc448",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Pyhton (learn-env)",
+   "language": "python",
+   "name": "learn-env"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
  "nbformat": 4,
  "nbformat_minor": 5
 }
