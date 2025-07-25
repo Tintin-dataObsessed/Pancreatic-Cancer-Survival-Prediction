7af5feee928909f763c887d1b174eaf6277c965a
Michelle Chekwoti <michellechekwoti@gmail.com>
2025-07-25 10:48:03 +0300

    trial

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
-import subprocess
-import sys
-
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-install("joblib")
-install("scikit-learn")
-install("xgboost")
 
 import streamlit as st
 import joblib
